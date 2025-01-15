3c8049e8a50e7270b206fb384469bf7678932758
Adam Jhansi <adamjhansi03@gmail.com>
2025-01-15 20:34:10 +0700

    update tampilan List Barang

--- lib/pages/list_barang.dart
+++ lib/pages/list_barang.dart
@@ -1,13 +1,41 @@
 import "package:flutter/material.dart";
 
+import "../components/card_header.dart";
+import "../components/text_styles.dart";
+
 class ListBarangPage extends StatelessWidget {
   const ListBarangPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
-      body: Center(
-        child: Text("LIST BARANG PAGE"),
+      appBar: AppBar(
+        // TODO : BUAT APP BAR YANG BAGUS
+        title: Text(
+          'Daftar Barang',
+          style: AppTextStyles.header(color: Colors.blueGrey),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: CardHeader(),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: CardHeader(),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: CardHeader(),
+            ),
+          ],
+        ),
       ),
     );
   }
